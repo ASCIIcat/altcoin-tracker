283d7a73d8c483aa45594eb7f791c8d31ad79b37
Jay <asciicatdesigns@gmail.com>
2017-03-14 11:31:39 +0200

    take 4

--- www/js/index.js
+++ www/js/index.js
@@ -1,28 +1,50 @@
+// $(document).ready(function(){
+//   $.jsonp({
+//     url: 'https://altcointrader.co.za/api/v1/live-stats',
+//     callbackParameter: 'callback',
+//     success: function(json) {
+//       var stats = JSON.parse(json);
+//       // BTC
+//       $('.prices').append('<li>BTC:<ul>');
+//       $('.prices').append('<li><strong>Price</strong>: '+stats.BTC.Price+'</li>');
+//       $('.prices').append('<li><strong>High</strong>: '+stats.BTC.High+'</li>');
+//       $('.prices').append('<li><strong>Low</strong>: '+stats.BTC.Low+'</li></ul>');
+//       // LTC
+//       $('.prices').append('<li>LTC:<ul>');
+//       $('.prices').append('<li><strong>Price</strong>: '+stats.LTC.Price+'</li>');
+//       $('.prices').append('<li><strong>High</strong>: '+stats.LTC.High+'</li>');
+//       $('.prices').append('<li><strong>Low</strong>: '+stats.LTC.Low+'</li></ul>');
+//       // NMC
+//       $('.prices').append('<li>NMC:<ul>');
+//       $('.prices').append('<li><strong>Price</strong>: '+stats.NMC.Price+'</li>');
+//       $('.prices').append('<li><strong>High</strong>: '+stats.NMC.High+'</li>');
+//       $('.prices').append('<li><strong>Low</strong>: '+stats.NMC.Low+'</li></ul>');
+//     },
+//     error: function(){
+//         $('.prices').append('<li>There was an error loading the feed');
+//     }
+//   });
+//
+// });
 $(document).ready(function(){
-  $.jsonp({
-    url: 'https://altcointrader.co.za/api/v1/live-stats',
-    callbackParameter: 'callback',
-    success: function(json) {
-      var stats = jQuery.parseJSON(json);
+  var altcoinAPI = "https://altcointrader.co.za/api/v1/live-stats";
+  $.getJSON( flickerAPI)
+    .done(function( data ) {
+      var stats = JSON.parse(data);
       // BTC
       $('.prices').append('<li>BTC:<ul>');
       $('.prices').append('<li><strong>Price</strong>: '+stats.BTC.Price+'</li>');
       $('.prices').append('<li><strong>High</strong>: '+stats.BTC.High+'</li>');
-      $('.prices').append('<li><strong>Low</strong>: '+stats.BTC.Low+'</li></ul>');
+      $('.prices').append('<li><strong>Low</strong>: '+stats.BTC.Low+'</li></ul></li>');
       // LTC
       $('.prices').append('<li>LTC:<ul>');
       $('.prices').append('<li><strong>Price</strong>: '+stats.LTC.Price+'</li>');
       $('.prices').append('<li><strong>High</strong>: '+stats.LTC.High+'</li>');
-      $('.prices').append('<li><strong>Low</strong>: '+stats.LTC.Low+'</li></ul>');
+      $('.prices').append('<li><strong>Low</strong>: '+stats.LTC.Low+'</li></ul></li>');
       // NMC
       $('.prices').append('<li>NMC:<ul>');
       $('.prices').append('<li><strong>Price</strong>: '+stats.NMC.Price+'</li>');
       $('.prices').append('<li><strong>High</strong>: '+stats.NMC.High+'</li>');
-      $('.prices').append('<li><strong>Low</strong>: '+stats.NMC.Low+'</li></ul>');
-    },
-    error: function(){
-        $('.prices').append('<li>There was an error loading the feed');
-    }
-  });
-
+      $('.prices').append('<li><strong>Low</strong>: '+stats.NMC.Low+'</li></ul></li>');
+    });
 });
