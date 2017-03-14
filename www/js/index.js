325265bc98fd51e7ea70fc8558eb7c54a87ace7a
Jay <asciicatdesigns@gmail.com>
2017-03-14 11:07:07 +0200

    trying with document ready

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -37,36 +20,39 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var receivedElement = parentElement.querySelector('.received');
-
-        $.jsonp({
-          url: 'https://altcointrader.co.za/api/v1/live-stats',
-          callbackParameter: 'callback',
-          success: function(json) {
-            var stats = jQuery.parseJSON(json);
-            // BTC
-            $('.received').append('<li>BTC:<ul>');
-            $('.received').append('<li><strong>Price</strong>: '+stats.BTC.Price+'</li>');
-            $('.received').append('<li><strong>High</strong>: '+stats.BTC.High+'</li>');
-            $('.received').append('<li><strong>Low</strong>: '+stats.BTC.Low+'</li></ul>');
-            // LTC
-            $('.received').append('<li>LTC:<ul>');
-            $('.received').append('<li><strong>Price</strong>: '+stats.LTC.Price+'</li>');
-            $('.received').append('<li><strong>High</strong>: '+stats.LTC.High+'</li>');
-            $('.received').append('<li><strong>Low</strong>: '+stats.LTC.Low+'</li></ul>');
-            // NMC
-            $('.received').append('<li>NMC:<ul>');
-            $('.received').append('<li><strong>Price</strong>: '+stats.NMC.Price+'</li>');
-            $('.received').append('<li><strong>High</strong>: '+stats.NMC.High+'</li>');
-            $('.received').append('<li><strong>Low</strong>: '+stats.NMC.Low+'</li></ul>');
-          },
-          error: function(){
-              $('.received').append('<li>There was an error loading the feed');
-          }
-      });
-
-        $('.received').setAttribute('style', 'display:block;');
-
     }
 };
+
+$(document).ready(function(){
+  var parentElement = document.getElementById(id);
+  var receivedElement = parentElement.querySelector('.received');
+
+  $.jsonp({
+    url: 'https://altcointrader.co.za/api/v1/live-stats',
+    callbackParameter: 'callback',
+    success: function(json) {
+      var stats = jQuery.parseJSON(json);
+      // BTC
+      $('.received').append('<li>BTC:<ul>');
+      $('.received').append('<li><strong>Price</strong>: '+stats.BTC.Price+'</li>');
+      $('.received').append('<li><strong>High</strong>: '+stats.BTC.High+'</li>');
+      $('.received').append('<li><strong>Low</strong>: '+stats.BTC.Low+'</li></ul>');
+      // LTC
+      $('.received').append('<li>LTC:<ul>');
+      $('.received').append('<li><strong>Price</strong>: '+stats.LTC.Price+'</li>');
+      $('.received').append('<li><strong>High</strong>: '+stats.LTC.High+'</li>');
+      $('.received').append('<li><strong>Low</strong>: '+stats.LTC.Low+'</li></ul>');
+      // NMC
+      $('.received').append('<li>NMC:<ul>');
+      $('.received').append('<li><strong>Price</strong>: '+stats.NMC.Price+'</li>');
+      $('.received').append('<li><strong>High</strong>: '+stats.NMC.High+'</li>');
+      $('.received').append('<li><strong>Low</strong>: '+stats.NMC.Low+'</li></ul>');
+    },
+    error: function(){
+        $('.received').append('<li>There was an error loading the feed');
+    }
+
+  });
+  $('.received').setAttribute('style', 'display:block;');
+
+});
